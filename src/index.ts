72e8439d6e17d1b4faf35150f0137ca1f3cbac02
Italo Cedro <italo.sales@cadmus.com.br>
2022-06-03 09:37:37 -0300

    chore: add ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ const files = (file: string) => file;
 files('test');
 
 console.log('Hello World');
-console.log('Hello World');
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -2,7 +2,4 @@ describe('test', () => {
   test('test', () => {
     expect(1).toBe(1);
   });
-  test('test', () => {
-    expect(2).toBe(2);
-  });
 });
