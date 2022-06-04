b53d70e78e0f94d6132c43b2f3e1c5101cf145cf
Italo Cedro <italo.sales@cadmus.com.br>
2022-06-04 12:24:27 -0300

    chore: adjustment jest config and package.json

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,20 @@
 const { resolve } = require('path');
 const root = resolve(__dirname);
 module.exports = {
+  rootDir: root,
   clearMocks: false,
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  preset: 'ts-jest',
+  testEnvironment: "node",
+  testMatch: [
+    '<rootDir>/src/**/*.spec.ts',
+    '<rootDir>/test/**/*.spec.ts',
+  ],
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/src/$1',
     '@test/(.*)': '<rootDir>/test/$1'
   },
-  preset: 'ts-jest',
-  rootDir: root,
-  testEnvironment: "node",
-  testMatch: [
-    '<rootDir>/test/**/*.spec.ts'
-  ],
 };
