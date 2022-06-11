bf9c8141b842fc02a54ece1b5d60422c52d8eb03
Italo Cedro <italo.sales@cadmus.com.br>
2022-06-11 16:03:53 -0300

    feat: create signup

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,22 @@ const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
   clearMocks: false,
+
+  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
   preset: 'ts-jest',
   testEnvironment: "node",
